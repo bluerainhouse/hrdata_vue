7588d66f24601f7307462122be3d36fd5407bad1
bluerainhouse <samba255256@gmail.com>
2023-08-08 15:05:19 +0800

    1505

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,43 @@
 <template>
-  <nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-      <div class="navbar-header">
-        <button
-          type="button"
-          class="navbar-toggle"
-          data-toggle="collapse"
-          data-target="#myNavbar"
-        >
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="/">進階血壓計量測分析服務</a>
-      </div>
-      <div class="collapse navbar-collapse" id="myNavbar">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="/">首頁</a></li>
-          <li><a href="/showUserRecord">量測記錄</a></li>
-          <li><a href="/graph">圖表分析</a></li>
-          <li><a href="/examplegraph">圖表範例</a></li>
-          <li><a href="/about">關於</a></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-          <li>
-            <a href="/login"
-              ><span class="glyphicon glyphicon-log-in"></span> 登入</a
-            >
-          </li>
-          <li><a href="/logout">登出</a></li>
-        </ul>
-      </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link to="/" class="navbar-brand"
+      >進階血壓計量測分析服務</router-link
+    >
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <router-link to="/about" class="nav-link">About</router-link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link disabled"
+            href="#"
+            tabindex="-1"
+            aria-disabled="true"
+            >Disabled</a
+          >
+        </li>
+      </ul>
     </div>
   </nav>
   <router-view />
 </template>
-
-<style>
-.navbar {
-  margin-bottom: 0;
-  border-radius: 0;
-}
-</style>
 
 <script></script>
