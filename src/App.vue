c3f7a8e92a0c7eac991a5184ef1019d674472ba8
bluerainhouse <samba255256@gmail.com>
2023-08-08 11:32:15 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button
+          type="button"
+          class="navbar-toggle"
+          data-toggle="collapse"
+          data-target="#myNavbar"
+        >
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="/">進階血壓計量測分析服務</a>
+      </div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="/">首頁</a></li>
+          <li><a href="/showUserRecord">量測記錄</a></li>
+          <li><a href="/graph">圖表分析</a></li>
+          <li><a href="/examplegraph">圖表範例</a></li>
+          <li><a href="/about">關於</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <a href="/login"
+              ><span class="glyphicon glyphicon-log-in"></span> 登入</a
+            >
+          </li>
+          <li><a href="/logout">登出</a></li>
+        </ul>
+      </div>
+    </div>
   </nav>
   <router-view />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.navbar {
+  margin-bottom: 0;
+  border-radius: 0;
 }
 </style>
+
+<script></script>
