f4098c92bf15cac0bf8022fd1137d00c87a20959
bluerainhouse <samba255256@gmail.com>
2023-08-09 14:04:34 +0800

    delete

--- src/views/RecordView.vue
+++ src/views/RecordView.vue
@@ -1,3 +1,57 @@
 <template>
-  <div>record</div>
+  <div>
+    <button @click="fetchData">Fetch Data</button>
+    <ul>
+      <li v-for="(record, index) in responseData" :key="index">
+        {{ record.i1 }} - {{ record.i2 }}
+        <button class="btn btn-danger" @click="deleteItem(record.id)">
+          Delete
+        </button>
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "RecordView",
+  data() {
+    return {
+      name: "",
+      responseData: [],
+    };
+  },
+  methods: {
+    fetchData() {
+      // 使用 Axios 發送 GET 請求
+      axios
+        .get("http://localhost:8080/allchart")
+        .then((response) => {
+          // 成功處理返回的數據
+          this.responseData = response.data;
+        })
+        .catch((error) => {
+          // 處理錯誤
+          console.error(error);
+        });
+    },
+    deleteItem(recordId) {
+      axios
+        .delete(`http://localhost:8080/${recordId}`)
+        // eslint-disable-next-line no-unused-vars
+        .then((response) => {
+          console.log(response.data);
+          this.fetchData(); // 刪除成功後刷新資料
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+  mounted() {
+    this.fetchData();
+  },
+};
+</script>
