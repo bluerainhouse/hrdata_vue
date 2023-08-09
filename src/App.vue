b2365aada5ff446d1c4f41332406692810d8c9e0
bluerainhouse <samba255256@gmail.com>
2023-08-09 17:36:02 +0800

    table

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,80 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <router-link to="/" class="navbar-brand">
-      進階血壓計量測分析服務
-    </router-link>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNav"
-      aria-controls="navbarNav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <router-link to="/about" class="nav-link">關於</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/record" class="nav-link">量測紀錄</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/graph-example" class="nav-link">
-            範例圖表
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a
-            class="nav-link disabled"
-            href="#"
-            tabindex="-1"
-            aria-disabled="true"
-          >
-            Disabled
-          </a>
-        </li>
-      </ul>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <router-link to="/" class="navbar-brand">
+        進階血壓計量測分析服務
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        @click="toggleNavbar"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div
+        class="collapse navbar-collapse"
+        id="navbarNav"
+        :class="{ show: isNavbarOpen }"
+      >
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/about" class="nav-link">關於</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/record" class="nav-link">量測紀錄</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/graph-example" class="nav-link">
+              範例圖表
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link disabled"
+              href="#"
+              tabindex="-1"
+              aria-disabled="true"
+            >
+              Disabled
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
+
   <router-view />
 </template>
 
-<script></script>
+<style>
+.container-fluid {
+  width: 80%;
+}
+</style>
+
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap";
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      isNavbarOpen: false,
+    };
+  },
+  methods: {
+    toggleNavbar() {
+      this.isNavbarOpen = !this.isNavbarOpen;
+    },
+  },
+};
+</script>
--- src/views/RecordView.vue
+++ src/views/RecordView.vue
@@ -1,16 +1,58 @@
 <template>
-  <div>
-    <button @click="fetchData">Fetch Data</button>
-    <ul>
-      <li v-for="(record, index) in responseData" :key="index">
-        {{ record.i1 }} - {{ record.i2 }}
-        <button class="btn btn-danger" @click="deleteItem(record.id)">
-          Delete
-        </button>
-      </li>
-    </ul>
+  <div class="container">
+    <div class="row">
+      <a class="btn btn-primary">查看全部紀錄</a>
+    </div>
+    <div class="row">
+      <table class="table table-hover" v-if="showTable">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">使用者ID</th>
+            <th scope="col">日期</th>
+            <th scope="col">收縮壓</th>
+            <th scope="col">舒張壓</th>
+            <th scope="col">脈搏</th>
+            <th scope="col">i1</th>
+            <th scope="col">i2</th>
+            <th scope="col">i3</th>
+            <th scope="col">詳細內容</th>
+            <th scope="col">刪除</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(record, index) in responseData" :key="index">
+            <td>#</td>
+            <td>{{ record.login }}</td>
+            <td>{{ record.recordDate }}</td>
+            <td>{{ record.highBp }}</td>
+            <td>{{ record.lowBp }}</td>
+            <td>{{ record.heartbeats }}</td>
+            <td>{{ record.i1 }}</td>
+            <td>{{ record.i2 }}</td>
+            <td>{{ record.i3 }}</td>
+            <td>
+              <a class="btn btn-success">詳細內容</a>
+            </td>
+            <td>
+              <a class="btn btn-danger" @click="deleteItem(record.id)">刪除</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div class="row" v-if="!showTable">
+      <h3>沒有量測記錄</h3>
+    </div>
   </div>
 </template>
+
+<style>
+.container {
+  width: 80%;
+}
+</style>
 
 <script>
 import axios from "axios";
@@ -21,6 +63,7 @@ export default {
     return {
       name: "",
       responseData: [],
+      showTable: false,
     };
   },
   methods: {
@@ -31,6 +74,7 @@ export default {
         .then((response) => {
           // 成功處理返回的數據
           this.responseData = response.data;
+          this.showTable = true;
         })
         .catch((error) => {
           // 處理錯誤
